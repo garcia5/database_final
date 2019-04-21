2a6c9d8472ab1f3e0e7b1af69991767344980338
garcia5 <garcia5@rpi.edu>
2019-04-21 14:54:26 -0400

    Application notes

---
+++ application.py
@@ -0,0 +1,14 @@
+"""
+- view weather for highest, lowest power consumption for a given city
+- min/max/average energy consumption for a given city over
+  - month
+  - year
+
+valid cities:
+  - chicago: AEP
+  - pittsburgh: duquesne
+  - philadelphia: ???
+  - new york: ???
+  - detroit: AEP
+  - indianapolis: AEP
+"""
